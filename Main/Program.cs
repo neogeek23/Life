6206eb24fe26b14b50a88ff2b210fba76a9260ea
neogeek23 <brad.stein@gmail.com>
2017-10-08 19:59:41 -0500

    Update Program.cs

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,14 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using N_Space;
 
-namespace Game_Of_Life___Console{
+namespace Life{
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Region game = new Region(2,10);
+            Console.WriteLine("This game of life has a max size of {0}, with a default state of {1}.", game.GetSize(), game.GetDefaultState());
+            game.AddInitialCoordinate(new int[] { 0, 0 }, State.Green);
+            game.AddInitialCoordinate(new int[] { 3, 2 }, State.Blue);
+            game.AddInitialCoordinate(new int[] { 9, 0 }, State.Red);
+            game.AddInitialCoordinate(new int[] { 0, 1 }, State.Red);
+            game.AddInitialCoordinate(new int[] { 0, 2 }, State.Blue);
+            game.AddInitialCoordinate(new int[] { 5, 5 }, State.Red);
+            game.AddInitialCoordinate(new int[] { 9, 9 }, State.Blue);
+            game.AddInitialCoordinate(new int[] { 4, 4 }, State.Green);
+            game.AddInitialCoordinate(new int[] { 7, 6 }, State.Green);
+            game.PrintSpace();
         }
     }
 }
